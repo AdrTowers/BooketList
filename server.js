a7ba02b1ac42ece5589fa3f58b1a3a0f36b4cebc
Bikash Bhaila <bikashbhaila@gmail.com>
2021-07-20 09:22:38 -0700

    updated books.js to handel post request

--- server.js
+++ server.js
@@ -35,12 +35,42 @@ server.post("/books", (req, res) => {
     })
 })
 
-// DELETE using id param
-server.delete("/books/:id", (req, res) => {
+// PUT
+server.put("/books", (req, res) => {
+    // extract param from the request
+    const { _id } = req.query;
 
-    bookObj.findByIdAndDelete(req.params.id).then(book => {
+    // checking if the put request has _id
+    if (_id === undefined) {
+        return res.status(400).send({ message: "?_id required" });
+    }
+
+    bookObj.findOneAndUpdate(_id, req.body).then(book => {
         if (!book) {
-            return res.status(404).send();
+            return res.status(404).send({ message: "no book with that _id found update" });
+        }
+        res.send({ status: "success" });
+    })
+        .catch(err => {
+            res.status(500).send(err);
+            console.log(err);
+        })
+})
+
+
+// DELETE using id param
+server.delete("/books", (req, res) => {
+    // extract param from the request
+    const { _id } = req.query;
+
+    // checking if the put request has _id
+    if (_id === undefined) {
+        return res.status(400).send({ message: "?_id required" });
+    }
+
+    bookObj.findByIdAndDelete(_id).then(book => {
+        if (!book) {
+            return res.status(404).send({ message: "no book with that _id found delete" });
         }
         res.send({ status: "success" });
     }).catch(err => {
